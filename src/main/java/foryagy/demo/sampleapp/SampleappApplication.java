3f51f4a3cc162bae3fdece05f652b6fc21158c0d
omar1218 <omar_1218@ymail.com>
2022-07-03 12:26:00 -0400

    Initial commit

---
+++ src/main/java/foryagy/demo/sampleapp/SampleappApplication.java
@@ -0,0 +1,15 @@
+package foryagy.demo.sampleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleappApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SampleappApplication.class, args);
+		System.out.println("hello world lol");
+	}
+
+}
